e95964ed62dc15c6de1a063c1d76e2f88ef12d58
Filip Dobrovolný <brnopcman@gmail.com>
2014-12-25 23:31:54 +0100

    ADDED: MapLoader into InGameScreenManager
    Implement Maploader.Map in InGameScreenManager

--- RTS/main/InGameScreenManager.py
+++ RTS/main/InGameScreenManager.py
@@ -1,8 +1,10 @@
 import pygame
 from pygame.locals import *
 from pygame.image import *
-from  RTS.main.isoMath import IsoMathHelper
+
+from RTS.main.isoMath import IsoMathHelper
 from RTS.main.GLTexture import Texture
+from RTS.main.MapLoader import Map
 '''
 Created on 25. 12. 2014
 
@@ -15,7 +17,7 @@ class InGameScreenManager(object):
     '''
 
 
-    def __init__(self, main, tileSizeH, tileSizeW):
+    def __init__(self, main, tileSizeH, tileSizeW, map="base"):
         '''
         Constructor
         '''
@@ -27,14 +29,14 @@ class InGameScreenManager(object):
         self.IsoMathHelper = None
         self._IsoMathHelperInit()
         
-        self.RegisterMapMovement()
         self.MapPosX, self.MapPosY =  0.0, 0.0
         self.MapMovConst = 10  
+        self.RegisterMapMovement()
         
-        #this stuff should be in future load by MapLoader
-        self.MapSize = (20, 20)
-        self.GroundMap = [[1 for i in range(self.MapSize[0])] for i in range(self.MapSize[1])]
-        self.tiles = [Texture("../res/map/grass.png")]
+        self.Map = Map(map)
+        self.Map.loadMap()
+        self.tiles = []
+        self.loadTextures()
         
         self.MouseSelectedTexture = Texture("../res/map/select_tile.png")
         self.MouseActive = True # False when user is in GUI
@@ -44,16 +46,15 @@ class InGameScreenManager(object):
         self.IsoMathHelper = IsoMathHelper(self.tileSizeW/2, self.tileSizeH/2, self.screenManager.width/2)
     
     def Draw(self):
-        for x in range(self.MapSize[0]):
-            for y in range(self.MapSize[1]):
-                #tile = self.GroundMap[y][x] - 1
-                tile = 0
+        for x in range(self.Map.sizeX):
+            for y in range(self.Map.sizeY):
+                tile = self.Map.matrix[y][x]
                 tile_x, tile_y = self.IsoMathHelper.Map2ScreenFIN((x,y), (self.MapPosX, self.MapPosY))
                 self.tiles[tile].draw(tile_x, tile_y)
         
         mouseCoord = self.IsoMathHelper.Screen2MapFIN(pygame.mouse.get_pos(), (self.MapPosX, self.MapPosY))
         
-        if  -1 < mouseCoord[0] <= self.MapSize[0] and -1 < mouseCoord[1] <= self.MapSize[1] and self.MouseActive:
+        if  -1 < mouseCoord[0] <= self.Map.sizeX and -1 < mouseCoord[1] <= self.Map.sizeY and self.MouseActive:
             select_x, select_y = self.IsoMathHelper.Map2ScreenFIN((int(mouseCoord[0]), int(mouseCoord[1])), (self.MapPosX, self.MapPosY))
             self.MouseSelectedTexture.draw(select_x, select_y)
     
@@ -89,4 +90,9 @@ class InGameScreenManager(object):
         self.main.EventHandler.registerKEYPRESSEDevent(K_DOWN, self.moveMapDown)
         self.main.EventHandler.registerKEYPRESSEDevent(K_LEFT, self.moveMapLeft)
         self.main.EventHandler.registerKEYPRESSEDevent(K_RIGHT, self.moveMapRight)
+    
+    def loadTextures(self):
+        print(self.Map.sizeX, self.Map.sizeX)
+        for TextureSrc in self.Map.textures:
+            self.tiles.append(Texture("../res/map/" + TextureSrc + ".png"))
     
